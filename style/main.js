0cbe14002e3411d5b3cda585b7812a10966a3c64
AsadbekNurberdiyev <asadbeknurberdiyev31@gmail.com>
2024-10-16 14:53:46 +0500

    java scriptda amal bajardim

---
+++ style/main.js
@@ -0,0 +1,138 @@
+             // 3-masala
+
+// let a = +prompt("sonni kiriting")
+// if(a > 0){
+//     a++;
+// } else if(a<0){
+//    a= a-2;
+// }else {
+//     a=10
+// }
+
+// console.log(a);
+
+
+              // 4-va 5-masalalar umumiy qildim
+
+// let a=+prompt("birinchi son")
+// let b=+prompt("ikkinchi son")
+// let c=+prompt("uchinchi son")
+// let t=0
+// let y=0
+// let l=0
+// if(a>0){
+//     t=t+1
+// }else if (a<0) {
+//     y=y+1
+// }else {
+//     l=l+1
+// }
+
+// if(b>0){
+//     t=t+1
+// }else if (b<0) {
+//     y=y+1
+// }else {
+//     l=l+1
+// }
+
+// if(c>0){
+//     t=t+1
+// }else if (c<0) {
+//     y=y+1
+// }else {
+//     l=l+1
+// }
+// console.log("musbat sonlat " +t+ " ta");
+// console.log("manfiy sonlar " +y+ " ta");
+// console.log("nolga tenglari " +l+ " ta");
+
+                //   6- va 7-masalalar umumiy qilingan
+
+// let a=+prompt("birinchi sonni kiriting")
+// let b=+prompt("ikkinchi sonni kiriting")
+// if (a>b) {
+//     console.log("birinchi son katta" +a);
+    
+// }else if (b>a) {
+//     console.log("ikkinchi son katta " +b);
+    
+// }else{
+//     console.log("ikkala son teng");
+    
+// }
+
+                //    8-masala
+
+// let a=+prompt("birinchi sonni kiriting")
+// let b=+prompt("ikkinchi sonni kiriting")
+// if (a>b) {
+//     console.log(a+" , " +b);   
+// }else {
+//     console.log(b+ " , " +a);
+// }
+
+                    //    9-masala
+
+// let A=+prompt("birinchi sonni kiriting")
+// let B=+prompt("ikkinchi sonn kiriting")
+// if (A>=B) {
+//     A=B-A
+// console.log(A+ " , " +B);
+// }else {
+//     console.log(A+ " , " +B);
+    
+// }
+
+                        // 10-masala
+
+
+// let A=+prompt("birinchi sonni kiriting")
+// let B=+prompt("ikkinchi sonn kiriting")
+// if (A!=B) {
+//     A=A+B
+//     B=A
+// }else {
+//     A=0
+//     B=0
+// }
+// console.log(A+ ", "+B);
+
+                        // 11-masala
+
+// let A=+prompt("birinchi sonni kiriting")
+// let B=+prompt("ikkinchi sonn kiriting")
+// if (A!=B) {
+//     if (A>B){
+//        B=A 
+//     }else {
+//         A=B
+//     }
+
+// }else {
+//     A=0
+//     B=0
+// }
+// console.log(A+ ", "+B);      
+
+                        // 12-masala
+
+// let a=+prompt("birinchi sonni kiriting")
+// let b=+prompt("ikkinchi sonni kiriting")
+// let c=+prompt("uchinchi sonni kiriting")
+// if (a<b) {
+//     if (b<=c) {
+//         console.log(a);
+        
+//     }else if (a<c) {
+//         console.log(a);
+//     }else {
+//         console.log(c);
+        
+//     }
+// }else if (b<c) {
+//     console.log(b);
+    
+// }else {
+//     console.log(c);
+// }
